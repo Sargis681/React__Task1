b06da2e9e2664173322cfb86aa3cd7f85fdc720f
Sargis681 <sargis.1998@mail.ru>
2023-07-27 18:53:53 +0400

    toolkit

--- number-book/src/components/store/formSlices/formSlice.js
+++ number-book/src/components/store/formSlices/formSlice.js
@@ -1,10 +1,10 @@
-// import { createSlice } from "@reduxjs/toolkit";
+import { createSlice } from "@reduxjs/toolkit";
 
-// const formSlices = createSlice({
-//   name: "form",
-//   initialState: [],
-//   reducers: {},
-// });
-// export const selectForm = (state) => state.form;
-// export const formReducer = formSlices.reducer;
-// export const {} = formSlices.actions;
+const formSlices = createSlice({
+  name: "form",
+  initialState: [],
+  reducers: {},
+});
+export const selectForm = (state) => state.form;
+export const formReducer = formSlices.reducer;
+export const {} = formSlices.actions;
