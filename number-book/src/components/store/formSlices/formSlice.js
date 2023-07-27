69d332730228c7b75376a0bf73f30543281f1902
Sargis681 <sargis.1998@mail.ru>
2023-07-27 18:30:52 +0400

    two-task

---
+++ number-book/src/components/store/formSlices/formSlice.js
@@ -0,0 +1,10 @@
+// import { createSlice } from "@reduxjs/toolkit";
+
+// const formSlices = createSlice({
+//   name: "form",
+//   initialState: [],
+//   reducers: {},
+// });
+// export const selectForm = (state) => state.form;
+// export const formReducer = formSlices.reducer;
+// export const {} = formSlices.actions;
