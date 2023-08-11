739df2e932efaa91f85ef752245444843e4ad819
Sargis681 <sargis.1998@mail.ru>
2023-08-11 17:54:41 +0400

    routing preparations

--- number-book/src/App.js
+++ number-book/src/App.js
@@ -1,16 +1,24 @@
+import { Suspense } from "react";
 import "./App.css";
 import AllLists from "./components/AllLists/AllLists";
 
 import Header from "./components/Header/Header";
 import LoginPage from "./components/LoginPage/LoginPage";
+import { Route } from "react-router";
+import Layout from "./components/Layout";
 
 function App() {
   return (
-    <div className="container">
-      {/* <Header /> */}
-      {/* <AllLists /> */}
-      <LoginPage />
-    </div>
+    <Suspense>
+      <div className="container">
+        {/* <Route path="/" element={<Layout />}> */}
+        {/* <Route index  /> */}
+        {/* <Header /> */}
+        {/* <AllLists /> */}
+        {/* </Route> */}
+        <LoginPage />
+      </div>
+    </Suspense>
   );
 }
 
---
+++ number-book/src/components/Layout.js
@@ -0,0 +1,11 @@
+import { Outlet } from "react-router-dom";
+import React from "react";
+// import Footer from "./Footer/Footer";
+// import Header from "./Header/Header";
+// import ToolBarNavigate from "./ToolBarNavigate/ToolBarNavigate";
+
+function Layout() {
+  return <>{/* <Outlet /> */}</>;
+}
+
+export default Layout;
